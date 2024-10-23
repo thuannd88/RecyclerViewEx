25ccb994f2e87622fe9c20daa376f3f8180641ad
Nguyen Dinh Thuan <thuan.nguyendinh@hust.edu.vn>
2024-10-23 13:26:03 +0700

    first commit

---
+++ app/src/main/java/com/example/recyclerviewex/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.recyclerviewex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rv: RecyclerView = findViewById(R.id.rv)
+        rv.layoutManager = LinearLayoutManager(this)
+
+        rv.adapter = MyAdapter(IntRange(0, 100).toList())
+    }
+}
+class MyAdapter(val data: List<Int>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>()
+{
+    class MyViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
+        val textView = row.findViewById<TextView>(R.id.number)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layout = LayoutInflater.from(parent.context).inflate(R.layout.item_view,
+            parent, false)
+        return MyViewHolder(layout)
+    }
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.textView.text = data.get(position).toString()
+    }
+    override fun getItemCount(): Int = data.size
+}
